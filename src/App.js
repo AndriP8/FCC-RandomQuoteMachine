57b2204f8b433747b18be2e7765f2a58cb2f61c0
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-05-10 15:28:15 +0700

    typo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 const quotes = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
 function App() {
-  const [qoute, setQuote] = useState("I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.");
+  const [qoute, setQuote] = useState("I have been impressed with the urgency of doing. Knowing is not enough we must apply. Being willing is not enough; we must do.");
   const [author, setAuthor] = useState("andLeonardo da Vinci");
   const [randomNumber, setRandomNumber] = useState(0);
   const [quotesArray, setQuotesArray] = useState(null);
